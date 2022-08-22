2d71eed5da8c07bf386332d2fa880980cec78ba0
annun <annun@DESKTOP-TUF8GF9>
2022-08-22 09:42:25 +0530

    Add project files.

---
+++ Matrix_Assingnment/Q1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Assingnment
+{
+    internal class Q1
+    {
+        public static void Main()
+        {
+            double principle = 1000 ,Amount=100000;
+            double  time;
+            // Calculate compound interest
+
+            time = Math.Log(Amount /principle) /   Math.Log(1+(0.05));
+
+            Console.Write("The time taken is " + time);
+            Console.ReadLine();
+        }
+    }
+}
